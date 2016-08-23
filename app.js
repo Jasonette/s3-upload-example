3e02126e13f8999d69b0ea720a2cf80e42ed9821
Gliechtenstein <ethan.gliechtenstein@gmail.com>
2016-08-23 12:02:01 -0400

    D

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var Post;
 var express = require('express');
 var aws = require('aws-sdk');
 var app = express();
@@ -7,246 +8,148 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var mongoose = require ("mongoose"); // The reason for this demo.
 
+/*************************************************************
+*
+* How this works
+* 
+* var jason:
+*   is the JSON stub that will be filled out
+*   and returned, depending on the result.
+*
+*   If everything goes well,
+*     the server will return jason["success"]
+*
+*   If something goes wrong,
+*     the server will return jason["error"]
+*
+* var init:
+*   takes care of initializations
+*
+*   init.db(): initializes mongodb
+*   init.server(): initializes express server
+*
+*************************************************************/
 
-
-/*
-var template = {
-  "$result": {
-    "head": {
-      "title": "image sample",
-      "data": {
-        "db": []
-      },
-      "actions": {
-        "$load": {
-          "type": "$set",
-          "options": {
-            "selected": ""
-          },
-          "success": {
-            "type": "$render"
+var jason = {
+  "error": {
+    "$jason": {
+      "head": "Error",
+      "body": {
+        "layers": [{
+          "type": "label",
+          "text": "Something went wrong",
+          "style": {
+            "top": "50%-50",
+            "left": "50%-100",
+            "width": "200",
+            "height": "100",
+            "font": "Avenir",
+            "size": "14"
           }
+        }]
+      }
+    }
+  },
+  "success": {
+    "$jason": {
+      "head": {
+        "title": "image sample",
+        "data": {
+          "db": []
         },
-        "$pull": {
-          "type": "$media.camera",
-          "options": {
-            "quality": "0.4"
-          },
-          "success": {
-            "type": "$network.upload",
+        "actions": {
+          "$load": {
+            "type": "$set",
             "options": {
-              "type": "s3",
-              "bucket": "fm.ethan.jason",
-              "path": "",
-              "sign_url": "https://imagejason.herokuapp.com/sign_url"
+              "selected": ""
             },
             "success": {
-              "type": "$network.request",
+              "type": "$render"
+            }
+          },
+          "$pull": {
+            "type": "$media.camera",
+            "options": {
+              "quality": "0.4"
+            },
+            "success": {
+              "type": "$network.upload",
               "options": {
-                "url": "https://imagejason.herokuapp.com/post",
-                "method": "post",
-                "data": {
-                  "bucket": "fm.ethan.jason",
-                  "path": "/",
-                  "filename": "{{$result.filename}}"
-                }
+                "type": "s3",
+                "bucket": "fm.ethan.jason",
+                "data": "{{$jason.data}}",
+                "path": "",
+                "sign_url": "https://imagejason.herokuapp.com/sign_url"
               },
               "success": {
-                "type": "$reload"
+                "type": "$network.request",
+                "options": {
+                  "url": "https://imagejason.herokuapp.com/post",
+                  "method": "post",
+                  "data": {
+                    "bucket": "fm.ethan.jason",
+                    "path": "/",
+                    "filename": "{{$jason.filename}}"
+                  }
+                },
+                "success": {
+                  "type": "$reload"
+                }
               }
             }
           }
-        }
-      },
-      "templates": {
-        "nav": {
-          "style": {
-            "theme": "dark"
-          }
         },
-        "body": {
-          "style": {
-            "border": "none"
+        "templates": {
+          "nav": {
+            "style": {
+              "theme": "dark"
+            }
           },
-          "sections": [
-            {
-              "type": "horizontal",
-              "style": {
-                "spacing": "0",
-                "padding": "0"
-              },
-              "header": {
-                "type": "vertical",
-                "style": {
-                  "align": "center",
-                  "padding": "20",
-                  "z_index": "-1"
-                },
-                "components": [{
-                  "type": "image",
-                  "url": "https://d30y9cdsu7xlg0.cloudfront.net/png/126349-200.png",
-                  "style": {
-                    "z_index": "-1",
-                    "width": "100"
-                  }
-                }]
-              },
-              "items": [
-                {
-                  "{{#if db && db.length > 0}}": {
-                    "{{#each db}}": {
-                      "type": "vertical",
-                      "style": {
-                        "width": "150",
-                        "height": "250"
-                      },
-                      "action": {
-                        "type": "$set",
-                        "options": {
-                          "selected": "{{url}}"
-                        },
-                        "success": {
-                          "type": "$render"
-                        }
-                      },
-                      "components": [{
-                        "type": "image",
-                        "style": {
-                          "width": "150"
-                        },
-                        "url": "{{url}}"
-                      }]
-                    }
-                  }
-                }, { 
-                  "{{#else}}": []
-                }
-              ]
-            }, 
-            [
+          "body": {
+            "style": {
+              "border": "none"
+            },
+            "sections": [
               {
-                "{{#if $get.selected}}": {
-                  "items": [{
+                "style": {
+                  "spacing": "0",
+                  "padding": "0"
+                },
+                "header": {
+                  "type": "vertical",
+                  "style": {
+                    "align": "center",
+                    "padding": "20",
+                    "z_index": "-1"
+                  },
+                  "components": [{
                     "type": "image",
-                    "url": "{{$get.selected}}",
+                    "url": "https://d30y9cdsu7xlg0.cloudfront.net/png/126349-200.png",
                     "style": {
-                      "width": "100%"
+                      "z_index": "-1",
+                      "width": "100"
                     }
                   }]
-                }
-              },
-              {
-                "{{#else}}": {
-                  "items": []
-                }
+                },
+                "items": [
+                  {
+                    "{{#if db && db.length > 0}}": {
+                      "{{#each db}}": {
+                        "type": "image",
+                        "style": {
+                          "width": "100%",
+                          "padding": "0"
+                        },
+                        "url": "{{url}}"
+                      }
+                    }
+                  }, { 
+                    "{{#else}}": []
+                  }
+                ]
               }
             ]
-          ]
-        }
-      }
-    }
-  }
-};
-*/
-var template = {
-  "$jason": {
-    "head": {
-      "title": "image sample",
-      "data": {
-        "db": []
-      },
-      "actions": {
-        "$load": {
-          "type": "$set",
-          "options": {
-            "selected": ""
-          },
-          "success": {
-            "type": "$render"
           }
-        },
-        "$pull": {
-          "type": "$media.camera",
-          "options": {
-            "quality": "0.4"
-          },
-          "success": {
-            "type": "$network.upload",
-            "options": {
-              "type": "s3",
-              "bucket": "fm.ethan.jason",
-              "data": "{{$jason.data}}",
-              "path": "",
-              "sign_url": "https://imagejason.herokuapp.com/sign_url"
-            },
-            "success": {
-              "type": "$network.request",
-              "options": {
-                "url": "https://imagejason.herokuapp.com/post",
-                "method": "post",
-                "data": {
-                  "bucket": "fm.ethan.jason",
-                  "path": "/",
-                  "filename": "{{$jason.filename}}"
-                }
-              },
-              "success": {
-                "type": "$reload"
-              }
-            }
-          }
-        }
-      },
-      "templates": {
-        "nav": {
-          "style": {
-            "theme": "dark"
-          }
-        },
-        "body": {
-          "style": {
-            "border": "none"
-          },
-          "sections": [
-            {
-              "style": {
-                "spacing": "0",
-                "padding": "0"
-              },
-              "header": {
-                "type": "vertical",
-                "style": {
-                  "align": "center",
-                  "padding": "20",
-                  "z_index": "-1"
-                },
-                "components": [{
-                  "type": "image",
-                  "url": "https://d30y9cdsu7xlg0.cloudfront.net/png/126349-200.png",
-                  "style": {
-                    "z_index": "-1",
-                    "width": "100"
-                  }
-                }]
-              },
-              "items": [
-                {
-                  "{{#if db && db.length > 0}}": {
-                    "{{#each db}}": {
-                      "type": "image",
-                      "style": {
-                        "width": "100%",
-                        "padding": "0"
-                      },
-                      "url": "{{url}}"
-                    }
-                  }
-                }, { 
-                  "{{#else}}": []
-                }
-              ]
-            }
-          ]
         }
       }
     }
@@ -255,86 +158,97 @@ var template = {
 
 
 
-var Post;
-var initDB = function(){
-  var uristring = process.env.MONGODB_URI;
-  var theport = process.env.PORT || 5000;
+var init = {
+  /**********************************************************************
+  *
+  * DB Initialization
+  * 
+  **********************************************************************/
+  db: function(){
+    var uristring = process.env.MONGODB_URI;
+    var theport = process.env.PORT || 5000;
 
-  mongoose.connect(uristring, function (err, res) {
-    if (err) {
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-    } else {
-      console.log ('Succeeded connected to: ' + uristring);
-    }
-  });
-  var postSchema = new mongoose.Schema({
-    url: String
-  });
-  Post = mongoose.model('posts', postSchema);
-};
-
-
-var initServer = function(){
-
-  var reload = function(res){
-    Post.find({}).sort({_id: -1}).exec(function(err, result) {
-      if (!err) {
-        // handle result
-        console.log("RESULT = ", result);
-        template["$jason"]["head"]["data"]["db"] = result;
-        res.json(template);
+    mongoose.connect(uristring, function (err, res) {
+      if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
       } else {
-        // error handling
-      };
+        console.log ('Succeeded connected to: ' + uristring);
+      }
     });
-  };
+    var postSchema = new mongoose.Schema({
+      url: String
+    });
+    Post = mongoose.model('posts', postSchema);
+  },
 
-  app.post('/post', function(req,res){
-    var url = "https://s3-us-west-2.amazonaws.com/" + req.body.bucket + req.body.path + req.body.filename;
-    var post = new Post({url: url});
-    post.save(function (err) {if (err) console.log ('Error on save!')});
-    reload(res);
-  });
-
-  app.get('/sign_url', function (req, res) {
-      aws.config.update({region: "us-west-2", endpoint: "https://s3-us-west-2.amazonaws.com", accessKeyId: process.env.S3_KEY, secretAccessKey: process.env.S3_SECRET});
-      var s3 = new aws.S3();
-
-      var s3_params = {
-          Bucket: req.query.bucket,
-          Key: req.query.path,
-          Expires: 60,
-          ACL: "public-read",
-          ContentType: req.query['content-type']
-      };
-      s3.getSignedUrl('putObject', s3_params, function(err, data){
-          if(err){
-              console.log(err);
-          }
-          else{
-              console.log("data = ", data);
-              res.json({"$jason": data});
-          }
+  /**********************************************************************
+  *
+  * Server Initialization
+  * 
+  **********************************************************************/
+  server: function(){
+    var reload = function(res){
+      Post.find({}).sort({_id: -1}).exec(function(err, result) {
+        // Fetch all items in the DB
+        if (!err) {
+          jason["success"]["$jason"]["head"]["data"]["db"] = result;
+          //res.json(jason["success"]);
+          res.json(jason["error"]);
+        } else {
+          // error handling
+          res.json(jason["error"]);
+        };
       });
-  });
-  app.get('/db', function(req, res){
-    Post.find({}).sort({_id: -1}).exec(function(err, result) {
-      if (!err) {
-        // handle result
-        console.log("RESULT = ", result);
-        template["$jason"]["head"]["data"]["db"] = result;
-        res.json({"response": template["$jason"]["head"]["data"]});
-      } else {
-        // error handling
-      };
-    });
-  });
-  app.get('/', function (req, res) {
-    reload(res);
-  });
+    };
 
-  app.listen(process.env.PORT || 3000);
+    // ROUTING
+    app.post('/post', function(req,res){
+      var url = "https://s3-us-west-2.amazonaws.com/" + req.body.bucket + req.body.path + req.body.filename;
+      var post = new Post({url: url});
+      post.save(function (err) {
+        if (err) {
+          console.log ('Error on save!');
+        }
+      });
+      reload(res);
+    });
+    app.get('/sign_url', function (req, res) {
+        aws.config.update({region: "us-west-2", endpoint: "https://s3-us-west-2.amazonaws.com", accessKeyId: process.env.S3_KEY, secretAccessKey: process.env.S3_SECRET});
+        var s3 = new aws.S3();
+        var s3_params = {
+            Bucket: req.query.bucket,
+            Key: req.query.path,
+            Expires: 60,
+            ACL: "public-read",
+            ContentType: req.query['content-type']
+        };
+        s3.getSignedUrl('putObject', s3_params, function(err, data){
+            if(err) {
+                console.log(err);
+                res.json(jason["error"]);
+            } else {
+                res.json({"$jason": data});
+            }
+        });
+    });
+    app.get('/db', function(req, res){
+      Post.find({}).sort({_id: -1}).exec(function(err, result) {
+        if (!err) {
+          jason["$jason"]["head"]["data"]["db"] = result;
+          res.json({"response": jason["success"]["$jason"]["head"]["data"]});
+        } else {
+          // error handling
+          res.json({"response": []});
+        };
+      });
+    });
+    app.get('/', function (req, res) {
+      reload(res);
+    });
+
+    app.listen(process.env.PORT || 3000);
+  }
 };
 
-initDB();
-initServer();
+init.db();
+init.server();

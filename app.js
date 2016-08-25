fd1a0f77e0d356c0147c28f7962812716138b38e
Gliechtenstein <ethan.gliechtenstein@gmail.com>
2016-08-25 12:08:22 -0400

    D

--- app.js
+++ app.js
@@ -111,29 +111,9 @@ var jason = {
             "header": {
               "menu": {
                 "text": "View Full Source",
-                "action": {
-                  "type": "$util.picker",
-                  "options": {
-                    "items": [{
-                      "text": "View JSON",
-                      "action": {
-                        "type": "$href",
-                        "options": {
-                          "url": "https://imagejason.herokuapp.com",
-                          "view": "web"
-                        }
-                      }
-                    }, {
-                      "text": "View full backend code",
-                      "action": {
-                        "type": "$href",
-                        "options": {
-                          "url": "https://github.com/Jasonette/s3-upload-example/blob/master/app.js",
-                          "view": "web"
-                        }
-                      }
-                    }]
-                  }
+                "href": {
+                  "url": "https://github.com/Jasonette/s3-upload-example/blob/master/app.js",
+                  "view": "web"
                 }
               }
             },
